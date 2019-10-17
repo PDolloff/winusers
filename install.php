88b46f494cd8b5c874f689e3482485ac95b45245
Peter Dolloff <Peter.Dolloff.1@gmail.com>
2019-10-17 15:45:46 -0500

    Made changes to make the extension compatible with OCS Inventory 2.6

--- install.php
+++ install.php
@@ -1,35 +1,31 @@
 <?php
-function plugin_version_winusers()
+
+/**
+ * This function is called on installation and is used to create database schema for the plugin
+ */
+function extension_install_winusers()
 {
-return array('name' => 'winusers',
-'version' => '1.2',
-'author'=> 'Guillaume PRIOU, Gilles DUBOIS',
-'license' => 'GPLv2',
-'verMinOcs' => '2.2');
+    $commonObject = new ExtensionCommon;
+    $commonObject -> sqlQuery("CREATE TABLE IF NOT EXISTS `winusers` (
+                             `ID` INT(11) NOT NULL AUTO_INCREMENT,
+                             `HARDWARE_ID` INT(11) NOT NULL,
+                             `NAME` VARCHAR(255) DEFAULT NULL,
+                             `LOGINTIME` datetime DEFAULT NULL,
+                             PRIMARY KEY  (`ID`,`HARDWARE_ID`)
+                             ) ENGINE=InnoDB ;");
+}
+/**
+ * This function is called on removal and is used to destroy database schema for the plugin
+ */
+function extension_delete_winusers()
+{
+    $commonObject = new ExtensionCommon;
+    $commonObject -> sqlQuery("DROP TABLE `winusers`;");
 }
 
-function plugin_init_winusers()
+/**
+ * This function is called on plugin upgrade
+ */
+function extension_upgrade_winusers()
 {
-$object = new plugins;
-$object -> add_cd_entry("winusers","other");
-
-$object -> sql_query("CREATE TABLE IF NOT EXISTS `winusers` (
- `ID` INT(11) NOT NULL AUTO_INCREMENT,
- `HARDWARE_ID` INT(11) NOT NULL,
- `NAME` VARCHAR(255) DEFAULT NULL,
- `LOGINTIME` datetime DEFAULT NULL,
- PRIMARY KEY  (`ID`,`HARDWARE_ID`)
- ) ENGINE=InnoDB ;");
-
 }
-
-function plugin_delete_winusers()
-{
-$object = new plugins;
-$object -> del_cd_entry("winusers");
-
-$object -> sql_query("DROP TABLE `winusers`;");
-
-}
-
-?>
